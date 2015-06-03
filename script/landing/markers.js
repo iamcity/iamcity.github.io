8299eb2d822afa0443afec9c14e21b058163610c
Marcos Garcia <magaum@gmail.com>
2015-06-03 10:51:54 +0200

    Using mock data

--- script/landing/markers.js
+++ script/landing/markers.js
@@ -1,7 +1,12 @@
-define(['landing/map'], function (map) {
-	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(-25.363882,131.044922),
-		map: map,
-		title: 'Hello World!'
-	});
+define(['landing/map', 'text!/data/activities.json'], function (map, activities) {
+	activities = JSON.parse(activities).activities;
+	for (activity in activities) {
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(
+				activities[activity].location.latitude, activities[activity].location.longitude
+			),
+			map: map,
+			title: [activities[activity].user.name, activities[activity].action.label].join(': ')
+		});
+	}
 });
