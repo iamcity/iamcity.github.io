b7a2f15fad4e63bb2257b3021161f77ce7a914b6
Kang-min Liu <gugod@gugod.org>
2015-06-03 16:21:58 +0200

    check facebook login on app start

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-define([ window ], function () {
+define(['jquery'], function ($) {
     var app = window.app = {};
 
     var FIREBASE_URL = "https://incandescent-inferno-2819.firebaseio.com/";
@@ -8,6 +8,30 @@ define([ window ], function () {
     app.firebase.onAuth(function(authData) {
         if (authData) {
             app.authData = authData;
+            $("[data-facebook-login]").text( app.authData.facebook.displayName );
+        }
+    });
+
+
+    $(function(){
+        if (app.authData && app.authData.facebook ) {
+            $("[data-facebook-login]").text( app.authData.facebook.displayName );
+            $("[data-facebook-login]").on("click", function(evt) {
+                $(this).text("Login");
+                app.firebase.unauth();
+                evt.preventDefault();
+                return false;
+            });
+        } else {
+            $("[data-facebook-login]").on("click", function(evt) {
+                evt.preventDefault();
+                app.firebase.authWithOAuthRedirect("facebook", function(error) {
+                    if (error) {
+                        console.log("Login Failed!", error);
+                    }
+                });
+                return false;
+            });
         }
     });
 
