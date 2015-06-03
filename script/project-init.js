ee07df7e4fbbb8390a6539f4d86773aa7108e9b8
Marcos Garcia <magaum@gmail.com>
2015-06-03 11:49:40 +0200

    Unifying form saving

--- script/project-init.js
+++ script/project-init.js
@@ -1,5 +1,6 @@
-require(['jquery', 'landing/project-init'], function ($) {
-	var form = $('[data-project-init-form]');
+require(['jquery'], function ($) {
+    var FIREBASE_URL = "https://incandescent-inferno-2819.firebaseio.com/";
+    var form = $("[data-project-init-form]");
 
 	form.on('submit', function (evt) {
 		evt.preventDefault();
@@ -11,8 +12,26 @@ require(['jquery', 'landing/project-init'], function ($) {
 		});
 
 		request.done(function (data) {
-			form.find('[name=projectLatitude]').val(data.results[0].geometry.location.lat);
-			form.find('[name=projectLongitude]').val(data.results[0].geometry.location.lng);
+			var p = {
+			    name: form.find("[name=projectName]").val(),
+			    description: form.find("[name=projectDescription]").val(),
+			    location: {
+			    	latitude: data.results[0].geometry.location.lat,
+			    	longitude: data.results[0].geometry.location.lng
+			    }
+			};
+
+			var ref = new Firebase( FIREBASE_URL );
+			ref.authAnonymously(function(error, authData) {
+			    if (error) {
+			        // console.log("Login Failed!", error);
+			    } else {
+			        // console.log("Authenticated successfully with payload:", authData);
+
+			        var projectRef = new Firebase( FIREBASE_URL + "/web/data/projects" );
+			        projectRef.push(p);
+			    }
+			});
 		})
 	});
 });
