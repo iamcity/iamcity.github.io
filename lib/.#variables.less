06793f0f5df2b39e8b255ab8935206986a47a7da
Stanislav Vlasov <Stan@5070B80C.static.ziggozakelijk.nl>
2015-06-04 02:36:41 +0200

    New look
    Buttons just for example

---
+++ lib/.#variables.less
@@ -0,0 +1,1 @@
+Stan@Stanislavs-MacBook-Air.local.994
--- script/landing.js
+++ script/landing.js
@@ -1,12 +1,25 @@
 define(['landing/map', 'text!/data/activities.json'], function (map, activities) {
-	activities = JSON.parse(activities).activities;
-	for (activity in activities) {
-		var marker = new google.maps.Marker({
-			position: new google.maps.LatLng(
-				activities[activity].location.latitude, activities[activity].location.longitude
-			),
-			map: map,
-			title: [activities[activity].user.name, activities[activity].action.label].join(': ')
-		});
-	}
+    activities = JSON.parse(activities).activities;
+    for (activity in activities) {
+	var icon = {
+            url: activities[activity].icon.file, //url
+	    scaledSize: new google.maps.Size(activities[activity].icon.size, activities[activity].icon.size) //size
+            //new google.maps.Size(activities[activity].icon.width, activities[activity].icon.height), //size
+            //new google.maps.Point(0,0), //origin
+            //new google.maps.Point(anchor_left, anchor_top) //anchor
+	    //url: "../res/sit_marron.png", // url
+	    //scaledSize: new google.maps.Size(50, 50), // scaled size
+	    //origin: new google.maps.Point(0,0), // origin
+	    //anchor: new google.maps.Point(0, 0) // anchor
+	};
+	var marker = new google.maps.Marker({
+	    position: new google.maps.LatLng(
+		activities[activity].location.latitude, activities[activity].location.longitude
+	    ),
+	    icon: icon,
+	    map: map,
+	    title: [activities[activity].user.name, activities[activity].action.label].join(': ')
+	    
+	});
+    }
 });
